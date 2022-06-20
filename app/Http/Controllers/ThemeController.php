2958f73390ac7487cb04a5893d259b7669abbed6
Thismood <ma@gmail.com>
2022-06-21 02:29:41 +0500

    Связал бэк ThemeController и Фронт

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -8,6 +8,50 @@ use Illuminate\Http\Request;
 class ThemeController extends Controller
 {
 
+    public function addText(Request $request)
+    {
+        $theme = Theme::where("ready", null)->first();
+        $allText = json_decode($theme->text);
+
+        if(isset($request->all()['text'])) {
+            if(isset($theme->text)) {
+                $allText->text[] = $request->all()['text'];
+            }
+        }
+        $theme->text = json_encode($allText);
+        $theme->update();
+        return $theme->text;
+    }
+
+    public function showText(Request $request)
+    {
+        $theme = Theme::where("ready", null)->first();
+        return $theme->text;
+    }
+
+    public function update(Request $request)
+    {
+        $theme = Theme::where("ready", null)->first();
+
+        if(isset($theme->text)) {
+            $allText = json_decode($theme->text);
+            if(isset($request->all()['id'])) {
+                $del_id = $request->all()['id'];
+
+                unset($allText->text[$del_id]);
+                sort($allText->text);
+
+            }
+
+            $theme->text = json_encode($allText);
+            $theme->update();
+        }
+        else {
+            $theme->text = json_encode(["text" => []]);
+        }
+        return $theme->text;
+    }
+
     public function index()
     {
         dd("ok");
@@ -17,19 +61,6 @@ class ThemeController extends Controller
     public function create()
     {
         dd("createok");
-    }
-
-    public function show($id)
-    {
-        return Theme::findOrFail($id);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $company = Theme::findOrFail($id);
-        $company->update($request->all());
-
-        return $company;
     }
 
     public function store(Request $request)
--- resources/js/components/PropComponent.vue
+++ resources/js/components/PropComponent.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Enter Name</div>
-
-                    <div v-for="url in data" class="card-body">
-                        {{ url.name }}
-                    </div>
-                </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Введите предложение</label>
+                <textarea v-model="textAdd" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
+            <button @click="add()" type="submit" class="btn btn-success">Добавить</button>
+        <ul class="list-group">
+            <li v-for="(item, index) in data.text" class="list-group-item d-flex justify-content-between align-items-center">
+                {{ item }} {{ index }}
+                <button @click="del(index)" type="button" class="btn btn-warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path>
+                    </svg>
+                </button>
+            </li>
+        </ul>
+        <div class="row">
+            <button @click="update()" class="btn btn-default text mb-1" v-if="!is_refresh">Обновить {{id}}</button>
+            <span class="badge badge-info mb-1" v-if="is_refresh">Обновление</span>
         </div>
     </div>
 </template>
@@ -18,18 +26,43 @@
     export default {
         data:function(){
             return{
-                data[],
+                data: [],
+                is_refresh: false,
+                id: 0,
+                textAdd: "",
+                params: [],
             }
         },
-        props: [
-            'data'
-        ],
         mounted() {
             this.update();
         },
         methods: {
+            add: function() {
+                console.log(this.params)
+                axios.post('/add-text-theme', {
+                    text: this.textAdd,
+                    //description: this.description
+                }).then((response) => {
+                    console.log(response.data)
+                    this.data = response.data
+                });
+            },
             update: function() {
-                console.log(this.data)
+                this.is_refresh = true;
+                axios.get('/show-text').then((response) => {
+                    console.log(response.data)
+                    this.data = response.data;
+                    this.is_refresh = false;
+                    this.id++;
+                });
+            },
+            del: function(id) {
+                axios.post('/update-text-theme', {
+                    id: id,
+                    //description: this.description
+                }).then((response) => {
+                    this.data = response.data
+                });
             }
         }
     }
