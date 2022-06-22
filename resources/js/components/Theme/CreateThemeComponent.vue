19767063b987160d9c0423510e5fb379c8fd006c
Thismood <ma@gmail.com>
2022-06-23 02:06:24 +0500

    Страница со списком модулей редактирования

--- resources/js/components/Theme/CreateThemeComponent.vue
+++ resources/js/components/Theme/CreateThemeComponent.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <SetThemeName :data="theme"></SetThemeName>
         <div class="d-flex justify-content-between align-items-center mb-3">
             <div class="d-flex justify-content-center  col-6">
                 <Progress ref="updateProgress"></Progress>
@@ -9,11 +8,11 @@
                 <button @click="setTextTheme()" type="button" class="btn btn-secondary m-1">Текст</button>
                 <button @click="setAudioTheme()" type="button" class="btn btn-secondary m-1">Аудио</button>
                 <button @click="setPictureTheme()" type="button" class="btn btn-secondary m-1">Изображения</button>
-                <button @click="setSettingsTheme()" type="button" class="btn btn-info m-1">Сохранить стиль</button>
+                <button @click="setSettingsTheme()" type="button" :disabled='saveButton' class="btn btn-primary m-1">Сохранить стиль</button>
             </div>
         </div>
 
-        <Component :is="currentComponent"></Component>
+        <Component @updPercent='getPercent' :is="currentComponent"></Component>
     </div>
 </template>
 
@@ -21,7 +20,6 @@
 
 import Progress from './Edit/ProgressCreateTheme'
 import SetTextTheme from './Edit/TextThemeComponent'
-import SetThemeName from './Edit/SetNameThemeComponent'
 
 Vue.component('set-text-theme', require('./Edit/TextThemeComponent').default);
 Vue.component('set-audio-theme', require('./Edit/AudioThemeComponent').default);
@@ -31,33 +29,52 @@ Vue.component('set-setting-theme', require('./Edit/SettingThemeComponent').defau
 export default {
     data:function(){
         return{
-            currentComponent: "set-text-theme"
+            currentComponent: "set-text-theme",
+            percent: 50,
+            saveButton: true,
         }
     },
-    // mounted() {
-    //     this.update();
-    // },
+    mounted() {
+        this.getPercent();
+    },
     components: {
         Progress,
-        SetThemeName,
         SetTextTheme
     },
     methods: {
         setTextTheme: function() {
             this.currentComponent = "set-text-theme";
-            this.$refs.updateProgress.update();
+            this.getPercent();
         },
         setAudioTheme: function() {
             this.currentComponent = "set-audio-theme";
-            this.$refs.updateProgress.update();
+            this.getPercent();
         },
         setPictureTheme: function() {
             this.currentComponent = "set-picture-theme";
-            this.$refs.updateProgress.update();
+            this.getPercent();
         },
         setSettingsTheme: function() {
             this.currentComponent = "set-setting-theme";
-            this.$refs.updateProgress.update();
+            this.getPercent();
+        },
+        cccOL(data) {
+            console.log(data)
+        },
+        getPercent: function() {
+            this.percent = 0;
+            axios.post('/how-ready', {
+                text: this.percent,
+                //description: this.description
+            }).then((response) => {
+                this.percent = response.data;
+                this.saveButton = true;
+                if(this.percent.data >= 62) {
+                    this.saveButton = false;
+                }
+                this.$refs.updateProgress.update();
+            });
+
         }
     }
 }
